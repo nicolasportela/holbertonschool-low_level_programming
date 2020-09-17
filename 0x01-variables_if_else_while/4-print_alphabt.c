84d64ed774adf5fbf81f1ac022210a7640772047
nicolasportela <2103@holbertonschool.com>
2020-09-17 20:59:31 +0000

    oaksjdlkj

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+char a[24] = {"abcdfghijklmnoprstuvwxyz"};
+
+for (n = 0; n < 24; n++)
+{
+putchar(a[n]);
+}
+{
+putchar('\n');
+}
+return (0);
+}
