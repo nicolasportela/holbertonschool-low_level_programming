4bf7191d60a9d5bd43276fb1f66496fe806da418
nicolasportela <2103@holbertonschool.com>
2020-09-17 21:46:03 +0000

    pqoiwe

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+char a[26] = {"zyxwvutsrqponmlkjihgfedcba"};
+
+for (n = 0; n < 26; n++)
+{
+putchar(a[n]);
+}
+{
+putchar('\n');
+}
+return (0);
+}
