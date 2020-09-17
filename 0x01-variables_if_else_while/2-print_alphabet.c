67421ffb6d5064c4e474d62a7092b093995ba9a6
nicolasportela <2103@holbertonschool.com>
2020-09-17 20:00:29 +0000

    kajsdh

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char a;
+a[25] = {"a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"};
+
+for (a = 0; ; a++)
+{
+putchar("a\n");
+}
+return (0);
+}
