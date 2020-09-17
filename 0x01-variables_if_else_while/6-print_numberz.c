d0c3c7c3b422c2977cbeb0cfc341ec963add47f6
nicolasportela <2103@holbertonschool.com>
2020-09-17 21:22:21 +0000

    qlkjsld

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+putchar(a[n]);
+}
+{
+putchar('\n');
+}
+return (0);
+}
