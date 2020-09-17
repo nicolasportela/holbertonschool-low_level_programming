afcfdc1c76426bdd3e0cd2ffce4d100f72718287
nicolasportela <2103@holbertonschool.com>
2020-09-17 21:05:40 +0000

    lkajsd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+char a[9] = {"123456789"};
+
+for (n = 0; n < 9; n++)
+{
+putchar(a[n]);
+}
+{
+putchar('\n');
+}
+return (0);
+}
