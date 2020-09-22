03462afec04a41046c997a64bb48a09c95dc4031
nicolasportela <2103@holbertonschool.com>
2020-09-22 14:42:51 +0000

    kamsdh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * void print_alphabet - check the code for Holberton School students.
+ *
+ * Return: void.
+ */
+
+void print_alphabet(void);
+{
+	char n = 'a';
+
+	for (n = 'a'; n = < 'z', n++)
+	{
+		_putchar('n');
+	}
+	_putchar('\n');
+return (0);
+}
