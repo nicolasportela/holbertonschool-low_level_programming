0a24725d4004f6fd5064e34536d2284d3bf286d3
nicolasportela <2103@holbertonschool.com>
2020-09-22 19:46:43 +0000

    lqkjeqw

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - check the code for Holberton School students.
+ * @x: name of the variable
+ * Return: void.
+ */
+
+int print_last_digit(int x)
+{
+	if (x < 0)
+	{
+		return (x % 10);
+	}
+	else
+	{
+		return (x);
+	}
+}
