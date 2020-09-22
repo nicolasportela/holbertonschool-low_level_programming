4c591f6ef49cc8d7e3c4217aac48a7c2d8299654
nicolasportela <2103@holbertonschool.com>
2020-09-22 22:08:56 +0000

    oqiwueqw

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ * @x: variable1
+ * @y: variable2
+ * Return: void.
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - check the code for Holberton School students.
+ *
+ * Return: void.
+ */
+
+void jack_bauer(void)
+{
+	int x;
+
+	for (x = 0; x <= 4; x++)
+	{
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
+}
