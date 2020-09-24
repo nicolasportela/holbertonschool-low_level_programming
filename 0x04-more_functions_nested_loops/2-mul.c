13e9302e94c9143f12d14eed275dca9a3d02ee54
nicolasportela <2103@holbertonschool.com>
2020-09-24 19:07:02 +0000

    lkajsd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ * @a: the name of the variable
+ * @b: the name of the variable
+ * Return: a * b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ *
+ * Return: void.
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
