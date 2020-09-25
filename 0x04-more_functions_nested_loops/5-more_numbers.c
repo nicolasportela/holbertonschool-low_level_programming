3e4fbdaa25f4ea6f0679512f062dacb8d401a858
nicolasportela <2103@holbertonschool.com>
2020-09-25 16:14:56 +0000

    laksjdh

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,16 @@
 void more_numbers(void)
 {
 	int a, b;
+
 	for (a = 0; a < 10; a++)
 	{
-		for (a = 0; a <= 14; a++)
+		for (b = 0; b <= 14; b++)
 		{
-			if (a > 9)
+			if (b > 9)
 			{
 				_putchar('1');
 			}
-			_putchar((a % 10) + '0');
+			_putchar((b % 10) + '0');
 		}
 		_putchar('\n');
 	}
