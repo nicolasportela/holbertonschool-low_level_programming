9b95f580050b44ab2c35e41a7f89ee02938d4dff
nicolasportela <70932338+nicolasportela@users.noreply.github.com>
2020-09-25 23:58:13 -0300

    Add files via upload
    asdhasdks

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print fizzbuzz
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+    int a;
+
+    for (a = 0; a <= 100; a++)
+    {
+        if ((a % 3) == 0)
+        {
+            printf("Fizz ");
+        }
+        else if ((a % 5) == 0)
+        {
+            if (a == 100)
+            {
+                printf("Buzz\n");
+            }
+            else
+            {
+                printf("Buzz ");
+            }
+        else if ((a % 3) == 0) && ((a % 5) == 0)
+        {
+            printf("FizzBuzz ");
+        }
+        else
+        {
+            printf("%i", a);
+        }
+    }
+    return (0);
+}
