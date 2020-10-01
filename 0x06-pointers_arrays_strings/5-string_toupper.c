606c95048da15f06e3662d7b6f561de2bc096521
nicolasportela <2103@holbertonschool.com>
2020-10-01 23:26:14 +0000

    alñskd

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *string_toupper - check lowercase.
+ * @s: s
+ * Return: char.
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}
