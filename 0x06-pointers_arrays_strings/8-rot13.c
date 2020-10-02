24c3d5e3d796698bc1cd4bb5c9a19df0095dbd91
nicolasportela <2103@holbertonschool.com>
2020-10-02 01:33:38 +0000

    lkajsd

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * rot13 - check the code for Holberton School students.
+ * @s: pointer to string
+ * Return: Always 0.
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+
+	char a[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char b[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
