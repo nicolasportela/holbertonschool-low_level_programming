ba2fcefc850eb4c3e37bd792c2b2ae0964c3692b
nicolasportela <2103@holbertonschool.com>
2020-10-05 20:22:50 +0000

    asd

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * *_strpbrk - check the code for Holberton School students.
+ * @s: variable name
+ * @accept: variable name
+ * Return: char.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return ('\0');
+}
