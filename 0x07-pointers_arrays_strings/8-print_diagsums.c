f023f79f7168b6ded64f3fc8a53e6f93f2598fac
nicolasportela <2103@holbertonschool.com>
2020-10-06 13:28:47 +0000

    lkajsd

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,14 +10,18 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, len, sig, sum1;
+	int i, sum1, sum2;
 
-	len = (size * size) - 1;
-	sig = size + 1;
+	sum1 = 0;
+	sum2 = 0;
 
-	for (i = 0; i < len; i = i + sig)
+	for (i = 0; i < size * size; i += size + 1)
 	{
-		sum1 = a[i] + a[sig];
-		printf("%d", sum1);
+		sum1 += a[i];
 	}
+	for (i = size - 1; i < size * size; i += size - 1)
+	{
+		sum2 += a[i];
+	}
+	printf ("%d, %d\n", sum1, sum2);
 }
