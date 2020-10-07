3a730dae8206f7fb684e88b8cb7bde38776bacff
nicolasportela <2103@holbertonschool.com>
2020-10-07 22:13:40 +0000

    asdj

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - check the code for Holberton School students.
+ * @n: variable name
+ * Return: int.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - check the code for Holberton School students.
+ * @n: variable name
+ * Return: int.
+ */
+
+int prime
+
+
+
+int is_prime_number(int n)
+{
+	if (n <= 1 || n % 2 == 0)
+	{
+		return (0);
+	}
+	else if (
+	{
+		return (1);
+	}
+}
