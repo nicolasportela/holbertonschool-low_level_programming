71cd6d7f15f9c113419e1c8fbaf447fdd714d06b
nico <nicop9@gmail.com>
2020-10-08 02:23:02 +0000

    kjashdkjh

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,23 +1,46 @@
 #include "holberton.h"
 
 /**
- * is_prime_number - check the code for Holberton School students.
- * @n: variable name
- * Return: int.
+ * _prime_check - Entry point
+ * @n: vriable name
+ * @i: vriable name
+ * Return: int
  */
 
-int prime
+int _prime_check(int n, int i)
+{
+        if (i >= n)
+        {
+                return (1);
+        }
+        if (n % i == 0)
+        {
+                return (0);
+        }
+        return (_prime_check(n, i + 2));
+}
 
-
+/**
+ * is_prime_number - Entry point
+ * @n: variable name
+ * Return: int
+ */
 
 int is_prime_number(int n)
 {
+	int x;
+
 	if (n <= 1 || n % 2 == 0)
 	{
 		return (0);
 	}
-	else if (
+	x = _prime_check(n, 3);
+	if (x != 0)
 	{
 		return (1);
+	}
+	else
+	{
+		return (0);
 	}
 }
