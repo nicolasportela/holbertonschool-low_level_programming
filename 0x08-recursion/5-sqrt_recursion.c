2739ec4f1e10973dfeb2c3fddba1a5f63709e0ce
nicolasportela <2103@holbertonschool.com>
2020-10-08 18:40:21 +0000

    lkjasd

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,33 @@
 #include "holberton.h"
-
 /**
- * _sqrt_recursion - check the code for Holberton School students.
- * @n: variable name
- * Return: int.
+ * increment - function to help
+ * @n: int
+ * @i: int
+ * Return: increment (Success)
+ */
+int increment(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	return (increment(n, i + 1));
+}
+/**
+ * _sqrt_recursion - function to help
+ * @n: int
+ * Return: increment (Success)
  */
 
 int _sqrt_recursion(int n)
 {
-	if (n
+	if (n <= 0)
+	{
+		return (-1);
+	}
+	return (increment(n, 1));
+}
