581ff210910f7d6c7e5e0fc8f4b8e53b0626551c
nicolasportela <2103@holbertonschool.com>
2020-10-14 20:19:50 +0000

    Second commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *str_concat - print the name of a program
+ * @s1: variable name
+ * @s2: variable name
+ * Return: char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int ii;
+	int j;
+	int jj;
+	char *s;
+
+
+	for (i = 0; i != '\0'; i++)
+	{
+	}
+	for (ii = 0; ii != '\0'; ii++)
+	{
+	}
+
+	s = malloc((i+ii) * sizeof(char) + 1);
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	if (s1 == '\0')
+	{
+		return (s1 = "");
+	}
+	if (s2 == '\0')
+	{
+		return (s2 = "");
+	}
+	for (j = 0; s1[j] != '\0'; j++)
+	{
+		s[j] = s1[j];
+	}
+
+	for (jj = 0; s2[jj] != '\0'; jj++)
+	{
+		s[j + jj] = s2[jj];
+	}
+	return (s);
+}
