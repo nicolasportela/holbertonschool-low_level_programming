44f7e0e68cefb550e9f875c8b10a8fa5384a7ef0
nicolasportela <2103@holbertonschool.com>
2020-10-14 21:50:29 +0000

    second commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - check the code for Holberton School students.
+ * @str: variable name
+ * Return: char
+ */
+
+char *_strdup(char *str)
+{
+	char *s; /*pointer to copy of str*/
+	int i; /*index of str*/
+	int ii; /*index of copy of str*/
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	s = malloc(i * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (ii = 0; ii < i; ii++)
+	{
+		s[ii] = str[ii];
+	}
+	return (s);
+}
