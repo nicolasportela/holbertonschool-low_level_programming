a73a7f91f23ab088ef2a4e4aff8379880efb89b5
nicolasportela <2103@holbertonschool.com>
2020-10-19 19:56:32 +0000

    second

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - print the name of a program
+ * @d:
+ * @name:
+ * @age:
+ * @owner:
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+	}
+}
