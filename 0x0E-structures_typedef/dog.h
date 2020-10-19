9c1b0653917c4a8d644f4e018c42b0e88cd9831e
nicolasportela <2103@holbertonschool.com>
2020-10-19 18:33:32 +0000

    first

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef PROJECT
+#define PROJECT
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _putchar(char c);
+
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
