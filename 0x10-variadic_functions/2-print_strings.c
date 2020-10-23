b416404929caab2e99425d862656ae8919919c75
nicolasportela <2103@holbertonschool.com>
2020-10-23 20:56:10 +0000

    second

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function
+ * @n: variable name
+ *
+ * Return: int
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
