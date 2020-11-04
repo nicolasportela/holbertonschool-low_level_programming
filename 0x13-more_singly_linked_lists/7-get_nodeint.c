2aa6b783edc3a68ed86bd379fef0f58ac2d8934b
nicolasportela <2103@holbertonschool.com>
2020-11-04 23:15:57 +0000

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,11 +9,17 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int node = 0;
+
 	if (head == NULL)
 	{
 		return (NULL);
 	}
 
-
-
+	while (node < index)
+	{
+		head = head->next;
+		node++;
+	}
+	return (head);
 }
