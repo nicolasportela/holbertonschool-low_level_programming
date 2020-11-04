8eecc8e4fbe75b812e044e967dc924582785d9b4
nicolasportela <2103@holbertonschool.com>
2020-11-04 22:45:47 +0000

    task 8

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,10 +4,22 @@
  * sum_listint - function
  * @head: variable
  *
- * Return: 
+ * Return: the sum of all the data
  */
 
 int sum_listint(listint_t *head)
 {
-	
+	int counter = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	while (head != NULL)
+	{
+		counter = (head->n) + counter;
+		head = head->next;
+	}
+	return (counter);
 }
