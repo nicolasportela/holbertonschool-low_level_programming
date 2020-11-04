7387ed566cc9f0b2f0e2a18dcb6b6ab36a06b562
nicolasportela <2103@holbertonschool.com>
2020-11-04 20:32:25 +0000

    tasks

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * *get_nodeint_at_index - function
+ * @head: variable
+ * @index: variable
+ * Return: the nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+
+
+}
---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function
+ * @head: variable
+ *
+ * Return: 
+ */
+
+int sum_listint(listint_t *head)
+{
+	
+}
