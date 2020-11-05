2f5e75504529408878d3d7dcf2c2489b223c50de
nicolasportela <2103@holbertonschool.com>
2020-11-05 00:23:01 +0000

    new files of tasks 9 and 10

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * *insert_nodeint_at_index - function
+ * @head: variable
+ * @idx: variable
+ * @n: variable
+ * Return: The adress of the new element
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
