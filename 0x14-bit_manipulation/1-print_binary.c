e1bc57104a4ff144ff67dbca0943e15ee38a635b
nicolasportela <2103@holbertonschool.com>
2020-11-05 23:48:04 +0000

    new files

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_binary - function
+ * @n: variable
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+
+	while (mask
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * get_bit - function
+ * @n: variable
+ * @index: variable
+ *
+ * Return: int
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
