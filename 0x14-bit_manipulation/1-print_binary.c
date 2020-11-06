9bb7060acd3cf662e12ea335a0858e9cdd7a6d0a
nicolasportela <2103@holbertonschool.com>
2020-11-06 02:31:09 +0000

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,6 +9,19 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int mask = 1;
+	unsigned int mask = 32768;
 
-	while (mask
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+		{
+			_putchar('0');
+
+		}
+		else
+		{
+			_putchar('1');
+		}
+		mask = mask >> 1;
+	}
+}
