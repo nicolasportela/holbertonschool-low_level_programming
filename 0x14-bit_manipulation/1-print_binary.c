4ff3f8e1d1797fe2e761d265a0933cf946cf2c2f
nicolasportela <2103@holbertonschool.com>
2020-11-06 02:33:40 +0000

    task 1 fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,19 +9,16 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned int mask = 32768;
-
-	while (mask > 0)
+	if (n > 1)
 	{
-		if ((n & mask) == 0)
-		{
-			_putchar('0');
-
-		}
-		else
-		{
-			_putchar('1');
-		}
-		mask = mask >> 1;
+		print_binary(n >> 1);
+	}
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
