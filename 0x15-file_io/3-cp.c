0845c882d5ad77c4ebdbb9b1ad90cc22e201943c
nicolasportela <2103@holbertonschool.com>
2020-11-10 21:21:55 +0000

    third

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,9 +2,50 @@
 
 /**
  * main - function
- *
+ * @argc: number of arguments
+ * @argv: string
  * Return: int
  */
 
-int main()
+int main(int argc, char *argv[])
 {
+	int fdorigin;
+	int fddest;
+	int count;
+	int wri;
+	char *buf = malloc(sizeof(char) * 1024);
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fdorigin = open(argv[1], O_RDONLY);
+	fddest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	if (fdorigin == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (fddest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	while (count > 0)
+	{
+		count = read(fdorigin, buf, 1024);
+		if (count == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buf);
+			exit(98);
+		}
+
+		if (count >= 1)
+		{
+wri 
