e130fd1a85c9b14e752334fb527ee6e357eb88d9
nicolasportela <2103@holbertonschool.com>
2020-11-11 00:19:17 +0000

    third

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,23 @@
 #include "holberton.h"
 
 /**
+ * close_func - function
+ * @a: variable
+ * Return: void.
+ */
+void close_func(int a)
+{
+	int close_a;
+
+	close_a = close(a);
+	if (close_a == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", a);
+		exit(100);
+	}
+}
+
+/**
  * main - function
  * @argc: number of arguments
  * @argv: string
@@ -9,43 +26,44 @@
 
 int main(int argc, char *argv[])
 {
-	int fdorigin;
-	int fddest;
-	int count;
-	int wri;
+	int fdorigin, fddest, fdwri, fdread = 1;
 	char *buf = malloc(sizeof(char) * 1024);
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-
 	fdorigin = open(argv[1], O_RDONLY);
 	fddest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	if (fdorigin == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	if (fddest == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-
-	while (count > 0)
+	while (fdread > 0)
 	{
-		count = read(fdorigin, buf, 1024);
-		if (count == -1)
+		fdread = read(fdorigin, buf, 1024);
+		if (fdread == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			free(buf);
-			exit(98);
+			free(buf), exit(98);
 		}
-
-		if (count >= 1)
+		if (fdread > 0)
 		{
-wri 
+			fdwri = write(fddest, buf, fdread);
+			if (fdwri == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				free(buf), exit(99);
+			}
+		}
+	}
+	close_func(fddest);
+	close_func(fdorigin);
+	free(buf);
+	return (0);
+}
