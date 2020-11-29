3c69ce57e851ba76af61d59298cf325ebee173e7
nicolasportela <2103@holbertonschool.com>
2020-11-29 00:07:36 +0000

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,10 +1,48 @@
 #include "holberton.h"
 
 /**
- * times_table - check the code for Holberton School students.
+ * times_table - prints the 9 times table, starting with 0.
  *
- * Return: 1 or 0.
+ * Return: void
  */
 
 void times_table(void)
 {
+	int i;
+	int j;
+	int m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			m = i * j;
+			if (m > 9)
+			{
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}
+			else
+			{
+				if (j == 0)
+				{
+					_putchar(m + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(m + '0');
+				}
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (j == 9)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
