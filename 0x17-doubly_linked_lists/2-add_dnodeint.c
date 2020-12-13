1568e58be84d985a8acdd8333915977dca80c5d4
nicolasportela <2103@holbertonschool.com>
2020-12-13 21:35:53 +0000

    task 2 fixed

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,10 +16,19 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 
+	/* if (*head == NULL)
+	 * {
+	 *	new->n = n;
+	 *	new->next = NULL;
+	 *	new->prev = NULL;
+	 *	*head = new;
+	 *      return (new);
+	 */
+
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
 	(*head)->prev = new;
-	new = *head;
+	*head = new;
 	return (new);
 }
