5415c1d0c93b02d1a11457dd2ffdd83b99384ebe
nicolasportela <2103@holbertonschool.com>
2020-12-14 03:59:07 +0000

    task 8 fixed

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,6 +9,7 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *current = *head;
 	dlistint_t *p_index = *head;
 	dlistint_t *p_prev = *head;
 	unsigned int i;
