21d7494019413c1a7b468d08c3b60d3489de7a48
nicolasportela <2103@holbertonschool.com>
2020-12-14 19:28:03 +0000

    advanced task 3

---
+++ 0x18-dynamic_libraries/my_functions.c
@@ -0,0 +1,64 @@
+#include "calc.h"
+
+/**
+ * add - sums two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - substracs two integer
+ * @a: integer 1
+ * @b: integer 2
+ * Return: result
+ */
+
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * div - divides two integers
+ * @a: integer 1
+ * @b: integer 2
+ * Return: result
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+
+/**
+ * mod - calculates module
+ * @a: integer 1
+ * @b: integer 2
+ * Return: result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
