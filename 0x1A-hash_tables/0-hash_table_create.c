60c310a7cc842ef8f8da1a825d79bcb9dcbf9abd
nicolasportela <2103@holbertonschool.com>
2021-01-16 02:29:20 +0000

    task 0 fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,12 +3,12 @@
 /**
  * hash_table_create - creates a hash table
  * @size: size of the array
- * Return: a pointer to the newly created hash table, or NULL if something wrong
+ * Return: a pointer to the newly created hash table, or NULL
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *table;
+	hash_table_t *table;
 	unsigned long int i = 0;
 
 	if (size == 0)
